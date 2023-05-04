83c1296ee9a8a4ddb433b19bdce996a6b201b652
Luximus <thomaslipman10@gmail.com>
2023-05-04 20:49:41 +0200

    Mod Setup

---
+++ SMC.Mod/Card/Switcheroo.cs
@@ -0,0 +1,78 @@
+﻿using SMC.Mod;
+using UnboundLib.Cards;
+using UnityEngine;
+
+namespace SMC.Cards
+{
+    class Switcheroo : CustomCard
+    {
+        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats,
+            CharacterStatModifiers statModifiers, Block block)
+        {
+            //Edits values on card itself, which are then applied to the player in `ApplyCardStats`
+        }
+
+        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data,
+            HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            (gun.reloadTime, gun.attackSpeed) = (gun.attackSpeed, gun.reloadTime);
+        }
+
+        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data,
+            HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats)
+        {
+            (gun.reloadTime, gun.attackSpeed) = (gun.attackSpeed, gun.reloadTime);
+        }
+
+        protected override string GetTitle()
+        {
+            return "Switcheroo";
+        }
+
+        protected override string GetDescription()
+        {
+            return "Swaps reload time and shooting cooldown";
+        }
+
+        protected override GameObject GetCardArt()
+        {
+            return null;
+        }
+
+        protected override CardInfo.Rarity GetRarity()
+        {
+            return CardInfo.Rarity.Uncommon;
+        }
+
+        protected override CardInfoStat[] GetStats()
+        {
+            return new[]
+            {
+                new CardInfoStat
+                {
+                    positive = true,
+                    stat = "Reload Speed",
+                    amount = "Shoot Cooldown",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                },
+                new CardInfoStat
+                {
+                    positive = true,
+                    stat = "Shoot Cooldown",
+                    amount = "Reload Speed",
+                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+                }
+            };
+        }
+
+        protected override CardThemeColor.CardThemeColorType GetTheme()
+        {
+            return CardThemeColor.CardThemeColorType.FirepowerYellow;
+        }
+
+        public override string GetModName()
+        {
+            return SphericalModificationCards.ModInitials;
+        }
+    }
+}
---
+++ SMC.Mod/SphericalModificationCards.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+using SMC.Cards;
+
+namespace SMC.Mod
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class SphericalModificationCards : BaseUnityPlugin
+    {
+        private const string ModId = "xyz.spherical.modification.cards";
+        private const string ModName = "Spherical Modification Cards";
+        public const string Version = "0.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "SMC";
+
+        public static SphericalModificationCards instance { get; private set; }
+        
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            Harmony harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            
+            CustomCard.BuildCard<Switcheroo>();
+        }
+    }
+}
